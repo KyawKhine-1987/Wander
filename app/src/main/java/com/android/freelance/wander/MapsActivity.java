a938a1360e167822fa23066c54ca07524f3e7503
KyawKhine <kknapster99@gmail.com>
2019-05-24 15:50:46 +0630

    done on May 21, 2019

--- app/src/main/java/com/android/freelance/wander/MapsActivity.java
+++ app/src/main/java/com/android/freelance/wander/MapsActivity.java
@@ -1,23 +1,57 @@
 package com.android.freelance.wander;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.StreetViewPanoramaOptions;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.PointOfInterest;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+
+import java.util.Locale;
+
+/*public class MapsActivity extends AppCompatActivity implements OnInfoWindowClickListener,OnMapReadyCallback {*/
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
-
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
     private static String LOG_TAG = MapsActivity.class.getName();
     private GoogleMap mMap;
 
@@ -27,9 +61,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        /*SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);*/
+        SupportMapFragment mapFragment = SupportMapFragment.newInstance();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, mapFragment).commit();
         mapFragment.getMapAsync(this);
     }
 
@@ -47,12 +86,54 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         Log.i(LOG_TAG, "TEST: onMapReady() called...");
 
-        mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        /*LatLng home = new LatLng(16.775267,  96.157636);
+        mMap.addMarker(new MarkerOptions().position(home).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(home));*/
+
+        mMap = googleMap;
+        float zoom = 15; /*1: World, 5: Landmass/continent, 10: City, 15: Streets, 20: Buildings*/
+
+        /*LatLng HOME = new LatLng(16.775267, 96.157636);*/
+        LatLng HOME = new LatLng(37.390220, -122.088427);// LatLng of Mountain View
+        /*Marker home = mMap.addMarker(new MarkerOptions().position(HOME).title("home").snippet("Population : 5,243,989"));*/
+        Marker Yangon = mMap.addMarker(new MarkerOptions().position(HOME).title("Yangon").snippet("Population : 5,243,989").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        /*Marker Yangon = mMap.addMarker(new MarkerOptions().position(HOME).title("Yangon").snippet("Population : 5,243,989").icon(vectorToBitmap(R.drawable.ic_location, Color.parseColor("#A4C639"))));*/
+        mMap.addMarker(new MarkerOptions().position(HOME).title("Home").icon(BitmapDescriptorFactory.defaultMarker
+                (BitmapDescriptorFactory.HUE_BLUE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(HOME, zoom));
+
+        setMapLongClick(mMap);
+        setPoiClick(mMap);
+        setMapStyle(mMap);
+        enableMyLocation(mMap);
+        setInfoWindowClickToPanorama(mMap);
+        /* // Set a listener for info window events.
+        mMap.setOnInfoWindowClickListener(this);*/
+
+      /*  GroundOverlayOptions homeOverlay = new GroundOverlayOptions()
+                .image(BitmapDescriptorFactory.fromResource(R.drawable.android))
+                .position(HOME, 100);
+        mMap.addGroundOverlay(homeOverlay);*/
+
+
+        // Add a circle, polygon and polyline  in Yangon
+       /* Circle circle = mMap.addCircle(new CircleOptions()
+                //.center(new LatLng(-33.87365, 151.20689))
+                .center(new LatLng(16.775267, 96.157636))
+                .radius(10000)
+                .strokeColor(Color.RED)
+                .fillColor(Color.BLUE));
+        // Add a triangle in the Gulf of Guinea
+        Polygon polygon = mMap.addPolygon(new PolygonOptions()
+                 .add(new LatLng(0, 0), new LatLng(0, 5), new LatLng(3, 5), new LatLng(0, 0))
+                .strokeColor(Color.RED)
+                .fillColor(Color.BLUE));
+        // Add a thin red line from London to New York.
+        Polyline line = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.5, -0.1), new LatLng(40.7, -74.0))
+                .width(5)
+                .color(Color.RED));*/
     }
 
     @Override
@@ -86,4 +167,163 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    private void setMapLongClick(final GoogleMap map) {
+        Log.i(LOG_TAG, "TEST: setMapLongClick() called...");
+
+       /*map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                map.addMarker(new MarkerOptions().position(latLng));
+            }
+        });*/
+
+        map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                Log.i(LOG_TAG, "TEST: onMapLongClick() called...");
+                String snippet = String.format(Locale.getDefault(),
+                        "Lat: %1$.5f, Long: %2$.5f",
+                        latLng.latitude,
+                        latLng.longitude);
+
+                map.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .title(getString(R.string.dropped_pin))
+                        .snippet(snippet)
+                        .icon(BitmapDescriptorFactory.defaultMarker
+                                (BitmapDescriptorFactory.HUE_BLUE))); //If you don't want to put that icon color then dropped pin default is red color.
+            }
+        });
+    }
+
+    private void setPoiClick(final GoogleMap map) {
+        Log.i(LOG_TAG, "TEST: setPoiClick() called...");
+
+        map.setOnPoiClickListener(new GoogleMap.OnPoiClickListener() {
+            @Override
+            public void onPoiClick(PointOfInterest poi) {
+                Marker poiMarker = mMap.addMarker(new MarkerOptions()
+                        .position(poi.latLng)
+                        .title(poi.name)
+                        .icon(BitmapDescriptorFactory.defaultMarker
+                                (BitmapDescriptorFactory.HUE_BLUE))); //If you don't want to put that icon color then dropped pin default is red color.
+                poiMarker.showInfoWindow();
+                poiMarker.setTag(getString(R.string.poi));
+            }
+        });
+    }
+
+    /**
+     * Loads a style from the map_style.json file to style the Google Map. Log
+     * the errors if the loading fails.
+     *
+     * @param googleMap The GoogleMap object to style.
+     */
+    private void setMapStyle(GoogleMap googleMap) {
+        Log.i(LOG_TAG, "TEST: setMapStyle() called...");
+
+        try {
+            // Customize the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.map_style));
+
+            if (!success) {
+                Log.e(LOG_TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(LOG_TAG, "Can't find style. Error: ", e);
+        }
+    }
+
+    private void enableMyLocation(GoogleMap map) {
+        Log.i(LOG_TAG, "TEST: enableMyLocation() called...");
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            map.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        Log.i(LOG_TAG, "TEST: onRequestPermissionsResult() called...");
+
+        // Check if location permissions are granted and if so enable the location data layer.
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                if (grantResults.length > 0
+                        && grantResults[0]
+                        == PackageManager.PERMISSION_GRANTED) {
+                    enableMyLocation(mMap);
+                    break;
+                }
+        }
+    }
+
+    /**
+     * Starts a Street View panorama when an info window containing the poi tag
+     * is clicked.
+     *
+     * @param map The GoogleMap to set the listener to.
+     */
+    private void setInfoWindowClickToPanorama(GoogleMap map) {
+        Log.i(LOG_TAG, "TEST: setInfoWindowClickToPanorama() called...");
+
+        map.setOnInfoWindowClickListener(
+                new GoogleMap.OnInfoWindowClickListener() {
+                    @Override
+                    public void onInfoWindowClick(Marker marker) {
+
+                        if (marker.getTag() == "poi") {
+                            // Set the position to the position of the marker
+                            StreetViewPanoramaOptions options =
+                                    new StreetViewPanoramaOptions().position(
+                                            marker.getPosition());
+
+                            SupportStreetViewPanoramaFragment streetViewFragment
+                                    = SupportStreetViewPanoramaFragment
+                                    .newInstance(options);
+
+                            // Replace the fragment and add it to the backstack
+                            getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.fragment_container,
+                                            streetViewFragment)
+                                    .addToBackStack(null).commit();
+                        }
+                    }
+                });
+    }
+
+    /* @Override
+    public void onInfoWindowClick(Marker marker) {
+        Log.i(LOG_TAG, "TEST: onInfoWindowClick() called...");
+
+        Toast.makeText(this, "Info window clicked",
+                Toast.LENGTH_SHORT).show();
+    }*/
+
+    /**
+     * Demonstrates converting a {@link Drawable} to a {@link BitmapDescriptor},
+     * for use as a marker icon.
+     */
+    /*private BitmapDescriptor vectorToBitmap(@DrawableRes int id, @ColorInt int color) {
+        Drawable vectorDrawable = ResourcesCompat.getDrawable(getResources(), id, null);
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),
+                vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        DrawableCompat.setTint(vectorDrawable, color);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }*/
 }
